9526c1edfb95beafecd03787e702c968d57421e5
Aleksandr <aleksandr-ishutin93@mail.ru>
2024-08-21 04:22:57 +0400

    01 Компонент часть 2

--- src/App.tsx
+++ src/App.tsx
@@ -3,36 +3,50 @@ import React from 'react';
 import './App.css';
 
 const App = () => {
+    console.log('App rendering')
     return (
         <div>
-            This is APP Component
+            <AppTitle/>
             <Rating/>
             <Accordion/>
+            <Rating/>
         </div>
     );
 }
+const AppTitle = () => {
+    return <>This is APP Component</>
+}
 const Rating = () => {
+    console.log('Rating rendering')
     return (
-        <div>
-          <Star/>
-          <Star/>
-          <Star/>
-          <Star/>
-          <Star/>
-        </div>
+        <>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+        </>
     )
 }
 const Accordion = () => {
+    console.log('Accordion rendering')
     return <div>
-        <h3>Меню</h3>
-        <ul>
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
-        </ul>
+        <AccordionTitle/>
+        <AccordionBody/>
     </div>
 }
+const AccordionTitle=()=> {
+    return <h3>Меню</h3>
+}
+const AccordionBody=()=> {
+    return <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+    </ul>
+}
 const Star = () => {
+    console.log('Star rendering')
     return <div>Star</div>
 }
 export default App;
