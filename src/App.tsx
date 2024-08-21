98fa1f5b0c07fa1b2acf766bc59870cebeb4e6f4
Aleksandr <aleksandr-ishutin93@mail.ru>
2024-08-21 05:04:47 +0400

    04 PropsType

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import './App.css';
 import {Accordion} from './components/Accordion/Accordion';
-import {Rating} from './components/Rating/Rating';
+
 
 const App = () => {
     console.log('App rendering')
