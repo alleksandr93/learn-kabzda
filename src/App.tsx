738e5fe871cfa4b0c5a01bcfb3b230388c3cfa92
Aleksandr <aleksandr-ishutin93@mail.ru>
2024-08-21 21:32:45 +0400

    08 CallBack 01

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,19 @@ import './App.css';
 import {OnOff} from './components/OnOff/OnOff';
 import {UnControlledAccordion} from './components/UnControlledAccordion/UnControlledAccordion';
 import {UnControlledRating} from './components/UnControlledRating/UnControlledRating';
+import {Rating, RatingValueType} from './components/Rating/Rating';
+import {Accordion} from './components/Accordion/Accordion';
 
 const App = () => {
+    const [ratingValue, setRatingValue] = React.useState<RatingValueType>(1);
+    const [accordionCollabsed, setAccordionCollabsed] = React.useState<boolean>(true)
     console.log('App rendering')
     return (
         <div>
-            {/*<Accordion titleValue={'Menu'} collapsed={true}/>*/}
-            {/*<Accordion titleValue={'Users'} collapsed={false}/>*/}
+            <Accordion titleValue={'Menu'} collapsed={accordionCollabsed}setAccordionCollapsed={setAccordionCollabsed}/>
+
             <OnOff/>
-        <UnControlledAccordion titleValue={'Menu'}/>
-     <UnControlledRating/>
+            <UnControlledRating/>
         </div>
     );
 }
--- src/components/Accordion/Accordion.tsx
+++ src/components/Accordion/Accordion.tsx
@@ -3,20 +3,23 @@ import React from 'react';
 type AccordionPropsType = {
     titleValue: string
     collapsed: boolean
+    setAccordionCollapsed: (collapsed: boolean) => void
 }
 export const Accordion = (props: AccordionPropsType) => {
     console.log('Accordion rendering')
     return <div>
-        <AccordionTitle title={props.titleValue}/>
+        <AccordionTitle title={props.titleValue}
+                        setAccordionCollapsed={() => props.setAccordionCollapsed(!props.collapsed)}/>
         {!props.collapsed && <AccordionBody/>}
     </div>
 }
 type AccordionTitlePropsType = {
     title: string
+    setAccordionCollapsed: () => void
 }
 const AccordionTitle = (props: AccordionTitlePropsType) => {
     console.log('AccordionTitle rendering')
-    return <h3>{props.title}</h3>
+    return <h3 onClick={() => props.setAccordionCollapsed()}>{props.title}</h3>
 }
 
 const AccordionBody = () => {
