55d2463ddcfbf39bcd04c0d5b164712c7e814129
Aleksandr <aleksandr-ishutin93@mail.ru>
2024-10-01 14:21:12 +0400

    20 Навороченный UseState

---
+++ src/stories/UseState.stories.tsx
@@ -0,0 +1,21 @@
+import {useMemo, useState} from 'react';
+
+export default {
+    title: 'UseState',
+}
+
+function generateData() {
+    console.log('generate data')
+    return 1
+}
+
+export const Exmple1 = () => {
+    console.log('Example1')
+    const [counter, setCounter] = useState(generateData)
+    const changer = ((state:number) => state + 1)
+    return <>
+        <button onClick={() => setCounter((prevState)=>prevState+1)}>+</button>
+        {/*<button onClick={() => setCounter(changer}>+</button>*/}
+        {counter}
+    </>
+}
